2bd918d71b08da0e4bb3b140678e0953a019e8e2
somya Thakur <somyathakur040403@gmail.com>
2026-06-17 12:34:24 +0530

    make create comment post method

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -5,7 +5,45 @@ const router = express.Router()
 const mongoose = require("mongoose")
 
 
+router.post("/create-comment", isLoggedIn, async(req, res) => {
+    try {
+            const foundUser = req.user
+            const { text, postId } = req.body
 
+              if (!text?.trim()) {
+                throw new Error("Comment text is required")
+              }
+
+              if (!postId) {
+              throw new Error("Post id is required")
+              } 
+
+              const post = await Post.findById(postId)
+
+              if (!post) {
+              throw new Error("Post not found")
+              }
+
+              const newComment = await Comment.create({
+              text,
+              authorId: foundUser._id,
+              postId
+              })
+
+              res.status(201).json({
+              success: true,
+              msg: "Comment added",
+              data: newComment
+              })
+  
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            err: error.message
+        })
+    }
+})
 
 module.exports = {
     commentRouter: router
